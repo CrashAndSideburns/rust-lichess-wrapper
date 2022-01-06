80c8b6385833720752c3a9234bc8f38f91192a2b
Morgan Arnold <morgan.r.arnold@outlook.com>
2022-01-06 12:30:02 -0800

    Add internal representation of user titles.

---
+++ src/lichess/title.rs
@@ -0,0 +1,20 @@
+use serde::{ Serialize, Deserialize };
+
+/// Representation of a Lichess user's title.
+/// Derived from [lila.user.Title][1].
+/// [1]: <https://github.com/ornicar/lila/blob/master/modules/user/src/main/Title.scala>
+#[derive(Serialize, Deserialize, Debug)]
+pub enum Title {
+    GM,
+    WGM,
+    IM,
+    WIM,
+    FM,
+    WFM,
+    CM,
+    WCM,
+    NM,
+    WNM,
+    LM,
+    BOT,
+}
