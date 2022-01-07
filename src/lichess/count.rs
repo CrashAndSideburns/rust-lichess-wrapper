eb5ab67cb033c10b92f74f0d1153e3804b9d79ea
Morgan Arnold <morgan.r.arnold@outlook.com>
2022-01-06 22:55:49 -0800

    Add internal representation of user game counts.
    Also add a mod.rs that I somehow forgot.

---
+++ src/lichess/count.rs
@@ -0,0 +1,22 @@
+use serde::{ Serialize, Deserialize };
+
+/// Represention of a Lichess user's overall game stats.
+/// Derived from [lila.user.Count][1].
+/// [1]: <https://github.com/ornicar/lila/blob/master/modules/user/src/main/Count.scala>
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct Count {
+    pub all: i32,
+    pub rated: i32,
+    pub ai: i32,
+    pub draw: i32,
+    pub draw_h: i32,
+    pub loss: i32,
+    pub loss_h: i32,
+    pub win: i32,
+    pub win_h: i32,
+    pub bookmark: i32,
+    pub playing: i32,
+    pub import: i32,
+    pub me: i32,
+}
