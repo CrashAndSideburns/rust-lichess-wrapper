0ba7e42723e8b5991e8d9f495137911eaf8ceff0
Morgan Arnold <morgan.r.arnold@outlook.com>
2022-01-06 19:28:05 -0800

    Add internal representation of user playtime.

---
+++ src/lichess/playtime.rs
@@ -0,0 +1,11 @@
+use serde::{ Serialize, Deserialize };
+
+/// Represention of a Lichess user's playtime.
+/// Derived from [lila.user.User.PlayTime][1].
+/// [1]: <https://github.com/ornicar/lila/blob/master/modules/user/src/main/User.scala>
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "camelCase")]
+pub struct PlayTime {
+    pub total: i32,
+    pub tv: i32
+}
